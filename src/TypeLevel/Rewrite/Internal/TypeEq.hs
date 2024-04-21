37692f5e41bb9d15dff769b5fc0fea452d41ba7d
Junji Hashimoto <junji.hashimoto@gree.net>
2024-04-21 14:34:39 +0900

    Fix codes for ghc-9.6

--- src/TypeLevel/Rewrite/Internal/TypeEq.hs
+++ src/TypeLevel/Rewrite/Internal/TypeEq.hs
@@ -2,7 +2,10 @@ module TypeLevel.Rewrite.Internal.TypeEq where
 
 import Data.Function
 
-#if MIN_VERSION_ghc(9,0,0)
+#if MIN_VERSION_ghc(9,6,0)
+import GHC.Plugins (Type)
+import GHC.Tc.Utils.TcType (eqType)
+#elif MIN_VERSION_ghc(9,0,0)
 import GHC.Plugins (Type, eqType)
 #else
 import GhcPlugins (Type, eqType)
